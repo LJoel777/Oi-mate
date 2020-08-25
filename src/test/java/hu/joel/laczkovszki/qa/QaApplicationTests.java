37766fd17b57a9e5c7de8c10a0738da2f5802a69
LJoel777 <szaknoel@gmail.com>
2020-08-25 16:04:26 +0200

    Inital commit

---
+++ src/test/java/hu/joel/laczkovszki/qa/QaApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.joel.laczkovszki.qa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
